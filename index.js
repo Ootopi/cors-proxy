bed182596b8df3ffa0fd69d772e28b02e2bda24e
Ootopi <ootopi.streams@gmail.com>
2022-07-31 19:21:25 +0800

    update headers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080
 const cors_proxy = require('cors-anywhere')
 cors_proxy.createServer({
     originWhitelist: ['https://ootopi.github.io/wheelerkk','https://ootopi.github.io'],
-    requireHeader: [],
+    requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, _ => {
     console.log(`Running CORS Proxy on ${host}:${port}`)
