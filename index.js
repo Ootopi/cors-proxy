0aab6d8dfe8aae9bcc3309cb1f8ac8dcda0591c6
Ootopi <ootopi.streams@gmail.com>
2022-07-31 19:01:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 8080
+const cors_proxy = require('cors-anywhere')
+cors_proxy.createServer({
+    originWhitelist: [],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, _ => {
+    console.log(`Running CORS Proxy on ${host}:${port}`)
+})
