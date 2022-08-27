d5df8b2bffab5eb21854f601e5246308479f29fd
Ootopi <94833201+Ootopi@users.noreply.github.com>
2022-08-27 18:27:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 8080
 const cors_proxy = require('cors-anywhere')
 cors_proxy.createServer({
-    originWhitelist: ['https://ootopi.github.io/wheelerkk','https://ootopi.github.io'],
+    originWhitelist: ['https://ootopi.github.io/wheelerkk', 'https://ootopi.github.io/cheropepero', 'https://ootopi.github.io'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, _ => {
